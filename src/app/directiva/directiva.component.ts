1ade15868991c23455fefc2ee2c7a1739aa2b337
luisfer19972020 <lcorrea.softsol@gmail.com>
2023-02-23 20:59:06 -0600

    Avance hasta el video 16

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+  listaCurso: string[] = ['Javascript', 'Typescript', 'Java SE', 'C#', 'Php'];
+}
